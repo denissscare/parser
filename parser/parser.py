1aa25ab715f2a51b30e33676326faf068725aa9d
Denis <denissscare@gmail.com>
2024-11-02 00:13:36 +0800

    update parser/ create new ft

--- parser/parser.py
+++ parser/parser.py
@@ -22,19 +22,38 @@ class HTMLParser(BaseParser):
                  soup: BeautifulSoup, 
                  tag: str, 
                  tag_class: str = "",
-                 tag_id:str = ""
-                 ) -> str:
+                 tag_id:str = "",
+                 ) -> str | list:
         
-        if tag_class: return soup.find_all(f'{tag}',{'class': f'{tag_class}'})
-        if tag_id: return soup.find_all(f'{tag}',{'id': f'{tag_class}'})
+        if tag_class: return self._get_class_data(soup=soup, tag=tag, tag_class=tag_class)
+        if tag_id: return self._get_id_data(soup=soup, tag=tag, tag_id=tag_id)
         
         return soup.find_all(f'{tag}')
     
-    def strip(self, soup: BeautifulSoup) -> list:
+    def strip(self, soup: BeautifulSoup) -> str:
         strip_data = [] 
         for el in soup:
             strip_data.append(el.text.strip())
-        return strip_data
+        return " ".join(el.text.strip() for el in soup)
+    
+    
+    def _get_href(self, link: BeautifulSoup) -> str:
+        return link.get('href')
+    
+    
+    def _get_class_data(self,
+                        soup: BeautifulSoup,
+                        tag: str,
+                        tag_class: str) -> list:
+        
+        return soup.find_all(f'{tag}',{'class': f'{tag_class}'})
+    
+    def _get_id_data(self,
+                     soup: BeautifulSoup,
+                     tag: str,
+                     tag_id: str) -> list:
+        
+        return soup.find_all(f'{tag}',{'id': f'{tag_id}'})
     
     def check_enclosure(self, ):
         pass
